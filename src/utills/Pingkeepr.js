9551eb4f801eaabeb424d1e051124ed5a0241812
Ridham-Savaliya <savaliyaridham46@.email.com>
2025-08-26 22:04:48 +0530

    FEAT:healthcheck route added!

---
+++ src/utills/Pingkeepr.js
@@ -0,0 +1,18 @@
+import { useEffect } from "react";
+
+export default function PingKeeper() {
+  useEffect(() => {
+    const interval = setInterval(() => {
+      fetch("https://api-quickbites.vercel.app/healthcheck", {
+        cache: "no-store",
+      })
+        .then(res => res.json())
+        .then(data => console.log("Pinged backend ✅:", data))
+        .catch(err => console.error("Ping failed ❌:", err));
+    }, 10 * 60 * 1000); // every 10 minutes
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return null;
+}
